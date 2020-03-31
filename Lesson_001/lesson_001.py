b2f7157ba22976143e9e9b80c9226c962e5128f0
nestor <nestorcolt@hotmail.com>
2020-03-31 20:53:07 +0200

    first class python SN

---
+++ Lesson_001/lesson_001.py
@@ -0,0 +1,67 @@
+# Variables
+##############################################################################################
+link_to_video = r"https://www.loom.com/share/21b1000ad71b40bea7f086a9aab4a510"
+
+x = 10
+y = 7
+
+result = x + y
+# print(result)
+
+# Variables more than numbers !
+some_string = "Hey there!"
+# print(some_string)
+
+string_a = "Hello"
+string_b = "World!"
+
+# print(string_a + string_b)
+# print(string_a + " " + string_b)
+
+# Booleans
+bool_one = True
+bool_two = False
+# print(bool_two)
+
+# None
+some_var = None
+# print(some_var)
+
+# User inputs
+# user_name = input("Please enter your name: ")
+# print("Hello " + user_name + "!")
+
+# Flow control (if/elif/else)
+# Mood checker
+mood = "scared"
+
+if mood == "happy":
+    print("It is great to see you happy!")
+
+elif mood == "nervous":
+    print("Take a deep breath 3 times.")
+
+elif mood == "scared":
+    print("Take a deep breath 10 times.")
+
+else:
+    print("Cheer up, mate!")
+
+##############################################################################################
+# Q&A
+mood = "sad"
+
+if mood == "happy":
+    print("input: happy")
+
+elif mood == "sad":
+    print("input: sad")
+
+elif mood == "angry":
+    print("input: sad")
+
+elif mood == "tears":
+    print("input: sad")
+
+else:
+    print("ELSE CONDITION!")
