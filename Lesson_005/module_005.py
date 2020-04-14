7253b9d92f2c127cfb49d4f467fdb7955e1bc9ba
nestor <nestorcolt@hotmail.com>
2020-04-14 20:36:05 +0200

    game development and lesson 005

---
+++ Lesson_005/module_005.py
@@ -0,0 +1,29 @@
+# Functions and reusability
+
+def say_hello():
+    print("Hello!")
+
+
+##############################################################################################
+# Without functions:
+
+# Greeting message
+# print("Hello my name is Nestor")
+
+# with functions:
+
+def greeting_message(username):
+    print("Hello my name is {}".format(username))
+
+
+""""
+
+Functions helps the code to be easy to maintain and to scale helping usability
+greeting_message(username="Nestor")
+greeting_message(username="Felipe")
+greeting_message(username="Mireia")
+greeting_message(username="Mitzy")
+
+"""
+
+##############################################################################################
